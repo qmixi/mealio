0413f7ae001e5f604b947f955e4df871ef1583fc
Piotr Kumorek <piotr.kumi@gmail.com>
2017-04-08 12:51:03 +0200

    mod

--- js/mealioViewModel.js
+++ js/mealioViewModel.js
@@ -2,6 +2,11 @@
 var MealioViewModel = function(data) {
 	var self = this;
 	self.mode = ko.observable("start");
+	self.form = ko.observable({
+		age: "",
+		growth: "",
+		weight: ""
+	});
 
 	self.goToForm = function()
 	{
